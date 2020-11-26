9c25e841ca13bd5b5607834f87bc1b0ae9c8d9fd
muslimadel <muslimadille@gmail.com>
2020-11-26 06:50:43 +0200

    pharmacy bootom bar

--- app/src/main/java/com/muslim_adel/sehatydoctors/modules/home/MainActivity.kt
+++ app/src/main/java/com/muslim_adel/sehatydoctors/modules/home/MainActivity.kt
@@ -2,6 +2,7 @@ package com.muslim_adel.sehatydoctors.modules.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.get
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomnavigation.LabelVisibilityMode
 import com.muslim_adel.sehatydoctors.R
@@ -11,6 +12,7 @@ import com.muslim_adel.sehatydoctors.modules.home.fragments.HomeFragment
 import com.muslim_adel.sehatydoctors.modules.home.fragments.OffersFragment
 import com.muslim_adel.sehatydoctors.modules.home.fragments.ProfileFragment
 import com.muslim_adel.sehatydoctors.remote.objects.doctor.DoctorProfileModel
+import com.muslim_adel.sehatydoctors.utiles.Q
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -18,13 +20,25 @@ class MainActivity : BaseActivity() {
     var navK=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            val fragment = HomeFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
-                .commit()
+        if(preferences!!.getString(Q.USER_TYPE,"")!=Q.USER_PHARM){
+            setContentView(R.layout.activity_main)
+            if (savedInstanceState == null) {
+                val fragment = HomeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+            }
+            initBottomNavigation()
+        }else{
+            setContentView(R.layout.activity_main2)
+            if (savedInstanceState == null) {
+                val fragment = HomeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+            }
+            initPharmacyBottomNavigation()
         }
-        initBottomNavigation()
+
+
     }
     private fun initBottomNavigation(){
 
@@ -37,6 +51,50 @@ class MainActivity : BaseActivity() {
                         .commit()
                     return@OnNavigationItemSelectedListener true
                 }
+                R.id.navigation_offers -> {
+                    val fragment = ProfileFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_appointment -> {
+
+                    val fragment =
+                        OffersFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_extras->{
+                    val fragment = ExstarsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        when (navK) {
+            0 -> {
+                bottomNavigationView.selectedItemId = R.id.navigation_home
+            }
+            1 -> {
+                bottomNavigationView.selectedItemId = R.id.navigation_offers
+            }
+            2 -> {
+                bottomNavigationView.selectedItemId = R.id.navigation_appointment
+            }
+            3 -> {
+                bottomNavigationView.selectedItemId = R.id.navigation_extras
+            }
+        }
+    }
+    private fun initPharmacyBottomNavigation(){
+
+        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
                 R.id.navigation_offers -> {
                     val fragment = ProfileFragment()
                     supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
@@ -62,9 +120,7 @@ class MainActivity : BaseActivity() {
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         when (navK) {
-            0 -> {
-                bottomNavigationView.selectedItemId = R.id.navigation_home
-            }
+
             1 -> {
                 bottomNavigationView.selectedItemId = R.id.navigation_offers
             }
